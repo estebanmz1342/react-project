d35d6f7992fffcd9bf6c42b023b9a5247525723e
DanielV <danielvaab@unisabana.edu.co>
2020-09-26 19:25:17 -0500

    Added rating

--- src/components/PagoComida/index.jsx
+++ src/components/PagoComida/index.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import Header from "../Header/Header";
-import {Comida} from "../Producto";
+import { Comida } from "../Producto";
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import "./pagina3.css"
 
+
+
+
 const PagoComida = () => {
+    
+    const [value, setValue] = React.useState(3);
+
     return (
         <>
             <div className="contenedor">
                 <Comida
                     src="https://instagram.fbog8-1.fna.fbcdn.net/v/t51.2885-15/e35/119379262_606890546648760_8852724859130531005_n.jpg?_nc_ht=instagram.fbog8-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=LozFolqDnKoAX9YUZ3U&_nc_tp=18&oh=38124fdec90a2b4f4bb6abf8f194d612&oe=5F99B049"
                     alt="Hamburguesa Zeta" name="Hamburgueseria Zeta"
-                    description="Hamburguesa Z tradicional" cantidad="Cantidad: 1" price="15k"/>
+                    description="Hamburguesa Z tradicional" cantidad="Cantidad: 1" price="15k" />
                 <a href="https://www.instagram.com" target="_blank">
                     <button onClick={() => window.alert("Vaya al perfil @HamburgueseriaZeta")}>PEDIR</button>
                 </a>
+                <Box component="fieldset" mb={3} borderColor="transparent">
+                    <Typography component="legend">Puntua :3</Typography>
+                    <Rating
+                        name="simple-controlled"
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    />
+                    </Box>
             </div>
 
 
