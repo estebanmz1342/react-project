e5365611a24448c479ec88267deaaec257dc24c9
danielvaab <danielvaab@unisabana.edu.co>
2020-09-28 17:24:28 -0500

    Css 5.0

--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Style from "./index.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return(
         <>
+            <div className={Style.container}>
             <h1 className={Style.myh1}>Login</h1>
             <form className={Style.form}>
-                <label>Correo</label>
-                <input type="text"/>
-                <label >Contraseña</label>
-                <input type="password"/>
-                <button type="submit">pagar ahora</button>
+                <label className={Style.text}>Correo: </label>
+                <input className={Style.input} type="text"/>
+                <label className={Style.text}>Contraseña: </label>
+                <input className={Style.input} type="password"/>
+                <Link to="/">
+                <button className={Style.buttonLogin} type="submit">Ingresar</button>
+                </Link>
             </form>
+            </div>
         </>
 
 
