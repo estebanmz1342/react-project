c3e013672baa26ffa2c5cb92c2882100608bb6c4
estebanmz1342 <juanestebanmunoz.jems@gmail.com>
2020-09-28 16:44:16 -0500

    login added

--- src/components/PagoAccesorio/index.jsx
+++ src/components/PagoAccesorio/index.jsx
@@ -5,6 +5,7 @@ import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Style from "./pagina3.module.css";
+import Login from "../../container/Login";
 
 
 
@@ -20,8 +21,8 @@ const PagoAccesorio = () => {
                     src="https://media.discordapp.net/attachments/691988704352272454/759583646909988884/120046530_183202300012695_3057444184010115438_n.png?width=499&height=499"
                     alt="Pulsera pareja" name="Pulseras Golden"
                     description="Pulseras Pareja Oro" cantidad="Cantidad: 1" price="220k" />
-                <a href="https://www.instagram.com" target="_blank">
-                    <button onClick={() => window.alert("Vaya al perfil @Goldenpulseras")}>PEDIR</button>
+                <a href={Login} target="_blank">
+                    <button onClick={() => window.alert("holi")}>PEDIR</button>
                 </a>
                 <Box className={Style.box} component="fieldset" mb={3} borderColor="transparent">
                     <Typography component="legend">Calificación</Typography>
--- src/container/Login/index.jsx
+++ src/container/Login/index.jsx
@@ -3,7 +3,18 @@ import Style from "./index.module.css";
 
 const Login = () => {
     return(
-        <h1 className={Style.myh1}>Login</h1>
+        <>
+            <h1 className={Style.myh1}>Login</h1>
+            <form className={Style.form}>
+                <label>Correo</label>
+                <input type="text"/>
+                <label >Contraseña</label>
+                <input type="password"/>
+                <button type="submit">pagar ahora</button>
+            </form>
+        </>
+
+
     );
 }
 export default Login;
